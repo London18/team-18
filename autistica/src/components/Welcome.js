a8f47c7247cc73e5fabd4380e760d470c1b0a728
Thanh Bui <thanhbui@Thanhs-MacBook-Pro.local>
2018-11-10 01:09:10 +0000

    added links - redirect hasn't worked

--- autistica/src/components/Welcome.js
+++ autistica/src/components/Welcome.js
@@ -1,33 +1,72 @@
 import React, { Component } from 'react';
 import logo from '../logo.svg';
 import './Welcome.css';
+import { BrowserRouter as Router, Link} from "react-router-dom";
+import { Redirect } from 'react-router';
 
-export default class Welcome extends Component {
+class Welcome extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {id: '',
+                redirect: false
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.goToDashboard = this.goToDashboard.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({id: event.target.value});
+  }
+
+  goToDashboard(event) {
+    if (this.state.id === '') {
+      alert('Please enter a proper ID.');
+      event.preventDefault();
+    } else {
+      this.setState({redirect: true});
+      event.preventDefault();
+    }
+  }
+
   render() {
+    if (this.state.redirect === true) {
+      return <Redirect to={{pathname: '/dahsboard'}} push />
+    }
+
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p> Welcome!!! </p>
-          <p> What's your <code> ID </code>? </p>
-          <p>
-            <form>
-              <label>
-                <input type="text" name="name" />
-              </label>
-            </form>
-          </p>
-          <p> <input type="submit" value="Let's start!" /> </p>
-          <a
+          Welcome!!!
+          <br/> 
+          <p>What's your <code>ID</code>?</p>
+          
+          <form onSubmit={this.goToDashboard}>
+            <label>
+              <input type="text" name="name" id={this.state.id} onChange={this.handleChange}/>
+            </label>
+          </form>
+          
+          <Router>
+            <div>
+              <Link to="/dashboard">
+                <input type="submit" name="Let's start" onClick={this.goToDashboard}/>
+              </Link>
+            </div>
+          </Router>
+          <p><a
             className="Autistica Website"
             href="https://www.autistica.org.uk/"
             target="_blank"
             rel="noopener noreferrer"
             color=""
           > Haven't got an <code>ID</code>? Create one now! 
-          </a>
+          </a></p>
         </header>
       </div>
     );
   }
 }
+
+export default Welcome;
